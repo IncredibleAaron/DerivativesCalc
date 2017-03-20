47e3e6371ff1bf59dd7d26e0c4d76c8389326a7f
Aaron <liqilinyeah@gmail.com>
2017-03-20 18:03:20 +0800

    Add files via upload

--- myFinLib.r
+++ myFinLib.r
@@ -1,34 +1,36 @@
 #Option pricing as all
-OptionPrice <- function(spot, mtrty, prd, sigma, strike, r, dvd=0, call=TRUE, US=FALSE) {
+OptionPrice <- function(spot, maturity, prd, sigma, strike, r, dvd=0, call=TRUE, US=FALSE) {
   
-  q <- Qcalc(r, dvd, mtrty, prd, sigma)
-  u <- Ucalc(sigma, mtrty, prd)
+  q <- Qcalc(r, dvd, maturity, prd, sigma)
+  u <- Ucalc(sigma, maturity, prd)
   
   if (!is.matrix(spot))
     {pTree <- bio.tree(spot, prd, u)}
   else
     {pTree <- spot}
   
-  OptionTree <- OptTree(pTree, strike, sigma, mtrty, r, prd, dvd, call=call, US=US)
+  OptionTree <- OptTree(pTree, strike, sigma, maturity, r, prd, dvd, call=call, US=US)
   
   result <- list(q=q, PriceTree=pTree, Payoff=OptionTree, Option.Price=OptionTree[1,1])
 }
 
 #Future Pricing
-FuturePrice <- function(spot, maturity, prd, sigma, r, dvd=0){
+FuturePrice <- function(lattice, maturity, prd, sigma, r, dvd=0){
+  
   q <- Qcalc(r, dvd, maturity, prd, sigma)
   u <- Ucalc(sigma, maturity, prd)
   
-  pTree <- bio.tree(spot, prd, u)
+  if(!is.matrix(lattice))
+    {lattice <- bio.tree(lattice, prd, u)}
   
   #future don't discount by interest rate
-  price <- discount(pTree, q, r=1, maturity)
+  price <- discount(lattice, q, r=1, maturity)
   
   future <- list(q=q, price.tree = price )
 }
 
 #Generate Binomial Tree lattice. Can be used for price/short rate generation. 
-bio.tree.simple <- function(start, prd, u){
+bio.tree.simple <- function(start, prd, u, d){
 
   b.tree <- matrix(0, prd+1, prd+1)
   b.tree[1,1] <- start
@@ -38,7 +40,7 @@ bio.tree.simple <- function(start, prd, u){
   }
   
   for (row in 2:(prd+1)){
-    b.tree[row,row:(prd+1)] <- b.tree[row-1,(row-1):prd]/u
+    b.tree[row,row:(prd+1)] <- b.tree[row-1,(row-1):prd]*d
   }
   
   return(b.tree)
@@ -69,11 +71,11 @@ bio.tree <- function(start, prd, u){
 
 #Calc with Strike price/Payoff Matrix
 
-OptTree <- function(pTree, strike, sigma, mtrty, r, prd, dvd = 0, call=TRUE, US=FALSE){ 
+OptTree <- function(pTree, strike, sigma, maturity, r, prd, dvd = 0, call=TRUE, US=FALSE){ 
   
-  r.real <-  exp(r*mtrty/prd)
+  r.real <-  exp(r*maturity/prd)
   
-  q <- Qcalc(r, dvd, mtrty, prd, sigma)
+  q <- Qcalc(r, dvd, maturity, prd, sigma)
   
 
   if (FALSE) {
@@ -107,13 +109,13 @@ else {
     payoff[payoff == strike] <- 0
 }
   
-payoff <- discount(payoff, q, r.real, mtrty, US)
+payoff <- discount(payoff, q, r.real, maturity, US)
 
 payoff
 
 }  
 #discount
-discount <- function(tree, q, r, maturity, early=FALSE) {
+discount <- function(tree, q, r, early=FALSE) {
   r.real <- r
   #r.real <-  exp(r*maturity/prd)
   #discounted <- matrix(0, prd+1, prd+1)
@@ -141,15 +143,24 @@ discount <- function(tree, q, r, maturity, early=FALSE) {
 }
 
 #Calc Up factor
-Ucalc <- function(sigma, mtrty, prd){
-  u <- exp(sigma*sqrt(mtrty/prd))
+Ucalc <- function(sigma, maturity, prd){
+  u <- exp(sigma*sqrt(maturity/prd))
   return(u)
 }
 
 #Calc q factor
-Qcalc <- function(r, dvd, mtrty, prd, sigma){
-  u <- exp(sigma*sqrt(mtrty/prd))
-  r.real <-  exp((r - dvd)*mtrty/prd)
+Qcalc <- function(r, dvd, maturity, prd, sigma){
+  u <- exp(sigma*sqrt(maturity/prd))
+  r.real <-  exp((r - dvd)*maturity/prd)
   q <- (r.real - 1/u)/(u-1/u)
   return(q)
+}
+
+#Calc Zero Coupon Bond price
+ZCB <- function(prd, r.start, u, d, bprice=100){
+  short.rate <- bio.tree.simple(r.start, prd, u, d)
+  bond.tree <- matrix(0, prd+1, prd+1)
+  bond.tree[, prd+1] <- bprice
+  bond.tree <- discount(bond.tree ,0.5, (short.rate+1))
+  return(bond.tree)
 }
